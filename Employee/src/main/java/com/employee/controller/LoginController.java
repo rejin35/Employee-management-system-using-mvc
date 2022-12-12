2236a517497bef6735cb331b8f70b2b9a0fbbc97
Hp <Hp@DESKTOP-U6KNCOL>
2022-12-12 11:00:18 +0530

    test 1

--- Employee/src/main/java/com/employee/controller/LoginController.java
+++ Employee/src/main/java/com/employee/controller/LoginController.java
@@ -8,7 +8,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 import com.employee.model.LoginUser;
 import com.employee.service.EmployeeService;
@@ -51,6 +50,4 @@ public class LoginController {
 		httpSession.invalidate();
 		return "loginpage";
 	}
-	
-
 }
